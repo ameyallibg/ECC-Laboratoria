b4909ca44929721890952b06789dfc4644cedb7b
ameyallibg <bgameyalli@gmail.com>
2019-09-21 14:07:16 -0500

    Subiendo ejercicio

--- Week1/ProblemSet/Sign.playground/Contents.swift
+++ Week1/ProblemSet/Sign.playground/Contents.swift
@@ -1,3 +1,61 @@
 import UIKit
 
-var str = "Hello, playground"
+
+
+enum Sign: String {
+   
+    case rock, paper, scissors
+    
+    var simpleDescription: String {
+        switch self {
+        case .rock:
+            return "👊"
+        case .paper:
+            return "🤚"
+        case .scissors:
+            return "✌️"
+    
+        }
+    }
+    func compare (computerSign:Sign) -> String {
+        if self == computerSign{
+            return "empate"
+        }
+        switch self {
+        case .rock:
+        if computerSign == .paper{
+            return "gana"
+            }
+        else if computerSign == .scissors{
+            return "pierde"
+            
+            }
+        case .paper:
+            if computerSign == .rock{
+                return "win"
+            }
+            else if computerSign == .scissors{
+                return "pierdo"
+                
+            }
+        case .scissors:
+            if computerSign == .rock{
+                return "pierdo"
+            }
+            else if computerSign == .paper{
+                return "ganas"
+                
+            }
+            
+            
+        }
+
+    
+
+
+}
+
+enum GameState: String {
+    case inicio, win , perder, empate
+}
+
